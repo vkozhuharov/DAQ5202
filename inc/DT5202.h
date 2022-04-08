80263876a6f52a642be73918551e3a7e84a1a942
Venelin Kozhuharov <vkozhuharov@gmail.com>
2022-04-08 13:20:29 +0300

    Communication with the DT5202 registers 	Board responds and performs the requested actions

--- inc/DT5202.h
+++ inc/DT5202.h
@@ -7,9 +7,28 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <cstring>
+
+#include "FERSlib.h"
+#include "FERS_Registers.h"
+
+#define Sleep(ms)				usleep((ms)*1000)
+
 
 #define COMMAND_PORT		"9760"  // Slow Control Port (access to Registers)
 #define STREAMING_PORT		"9000"	// Data RX Port (streaming)
+
+#define ETH_BLK_SIZE  (1024)						// Max size of one packet in the recv 
+#define RX_BUFF_SIZE  (1024*1024)					// Size of the local Rx buffer
+
+// #define ENABLE_FERSLIB_LOGMSG 0
+// #define FERS_CONNECT_TIMEOUT		3		// timeout connection in seconds
+
+
+//#define FERS_LibMsg printf
+#define SHUT_SEND	1
+#define SHUT_BOTH	2
+
 
 //Ethernet defines
 typedef int							f_socket_t;			//!< Return type of socket(). On Linux socket() returns int.
@@ -28,13 +47,35 @@ class DT5202 {
   void Init(){;};
   int Init(const char*, const char*);
   void Run(){;};
-  void Close(){;};
+  void Close();
+
+  int LLeth_OpenDevice(const char *board_ip_addr);
+  int LLeth_CloseDevice();
+
+  f_socket_t LLeth_ConnectToSocket(const char *board_ip_addr, char *port);
+  int LLeth_WriteRegister(uint32_t address, uint32_t data);
+  int LLeth_ReadRegister(uint32_t address, uint32_t *data);
+  
+  // -----------------------------------------------------------------------------------
+  // According to CAEN:   R/W Reg via I2C (old version, slow... don't use it)
+  // -----------------------------------------------------------------------------------
+  int LLeth_ReadReg_i2c(uint32_t address, uint32_t *data);
+  int LLeth_WriteReg_i2c(uint32_t address, uint32_t data);
+  
+  // -----------------------------------------------------------------------------------
+  // According to CAEN:   R/W Reg via SPI (new version, faster than I2C)
+  // -----------------------------------------------------------------------------------
+  int LLeth_ReadMem(uint32_t address, char *data, uint16_t size);
+  int LLeth_WriteMem(uint32_t address, char *data, uint16_t size);
+  
+  int LLeth_ReadData(char *buff, int size, int *nb);
 
  private:
   void *ptr;
   int handle;
   f_socket_t CtrlSocket;
   f_socket_t DataSocket;
+  FERS_BoardInfo_t BoardInfo;
   char *ethRxBuff;
   uint32_t ethBuff_wp;
   uint32_t ethBuff_rp;
